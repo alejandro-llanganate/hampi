ade6758ae1cae4246aeede93d9d77b7a4eb31bed
alejandrollanganate <alejandrollanganate@gmail.com>
2020-04-16 21:46:34 -0500

    Updates

---
+++ precache-manifest.8f27573a9eebc619a10051b87a3ed712.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "098ae96034d611bd54d2882abdcc1528",
+    "url": "/hampi/index.html"
+  },
+  {
+    "revision": "49b75273e8f4473aaa46",
+    "url": "/hampi/static/css/main.41194154.chunk.css"
+  },
+  {
+    "revision": "637c31b2adb78a9bf867",
+    "url": "/hampi/static/js/2.f99eecc5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/hampi/static/js/2.f99eecc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b75273e8f4473aaa46",
+    "url": "/hampi/static/js/main.9fcf7a65.chunk.js"
+  },
+  {
+    "revision": "0cec5883d4e0a2d9c4af",
+    "url": "/hampi/static/js/runtime-main.3f3085dd.js"
+  },
+  {
+    "revision": "cd6fe0a7e9947bf73c15508b4748ef1f",
+    "url": "/hampi/static/media/LOGOhAMPI.cd6fe0a7.png"
+  },
+  {
+    "revision": "b362b0c4f6a8d67fcff98407e31fddf0",
+    "url": "/hampi/static/media/bata.b362b0c4.png"
+  },
+  {
+    "revision": "e775735bf152ead34dd5a9a53ff3625c",
+    "url": "/hampi/static/media/bataSimple.e775735b.png"
+  },
+  {
+    "revision": "1fdb0a797f9ef135c421db5a38b0428d",
+    "url": "/hampi/static/media/cubrecabezas.1fdb0a79.png"
+  },
+  {
+    "revision": "3ea8fb2f3845cc5fb7bfda322374ce90",
+    "url": "/hampi/static/media/cubrezapatos.3ea8fb2f.png"
+  },
+  {
+    "revision": "f358f3768bcc2f50dadfd6007a6d8282",
+    "url": "/hampi/static/media/gelVaporal.f358f376.png"
+  },
+  {
+    "revision": "5c4d23c40e7bb42df0d0931cb98b47b8",
+    "url": "/hampi/static/media/gelisabella.5c4d23c4.png"
+  },
+  {
+    "revision": "71459fab83e2646226a715cc6b4b6270",
+    "url": "/hampi/static/media/jabónSahara.71459fab.png"
+  },
+  {
+    "revision": "d540f2f70f46a12b4e13c9eae4b38848",
+    "url": "/hampi/static/media/mascarillaKN95.d540f2f7.png"
+  },
+  {
+    "revision": "cd066d4c9f756a353b8378bcf2c7991d",
+    "url": "/hampi/static/media/mascarillaQuirurgica.cd066d4c.png"
+  },
+  {
+    "revision": "dafaa6241c53ac3bcedc329d2f1b48d5",
+    "url": "/hampi/static/media/mascarillaSimple.dafaa624.png"
+  },
+  {
+    "revision": "905556c29edbbd6c4aa32fa03c72fc0d",
+    "url": "/hampi/static/media/modelohampi1.905556c2.png"
+  },
+  {
+    "revision": "e524ab27321369c506de1e362419bee1",
+    "url": "/hampi/static/media/overall1.e524ab27.png"
+  },
+  {
+    "revision": "0e7b2e8e6beb282947ceb2add6fc7200",
+    "url": "/hampi/static/media/overall2.0e7b2e8e.png"
+  },
+  {
+    "revision": "a4e6bde47fd9a8157787830a87bb2c02",
+    "url": "/hampi/static/media/overallAntifluidos.a4e6bde4.png"
+  }
+]);
